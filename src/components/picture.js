6daa58aceb1a2cef47c3c4a2f9f79419db37c55f
Desislava Shunturova <shunturova@gmail.com>
2020-01-27 17:22:30 -0500

    picture component updated

--- src/components/picture.js
+++ src/components/picture.js
@@ -4,32 +4,97 @@ import { firestore, storage } from '../firebase';
 class Picture extends Component {
   state = {
     img: '',
-    urlArray: []
+    urlArray: [],
+    pictureNames: []
   };
 
   componentDidMount = async () => {
-    let newArr = [];
+    this.setState({ pictureNames: this.props.pictureNames });
+    //     let newArr = [];
+    //     const images = storage.ref().child(this.props.address);
+    //     //DOC trial
+    //     // let str = '';
+    //     // storage
+    //     //   .ref()
+    //     //   .child('Testing')
+    //     //   .getDownloadURL()
+    //     //   .then(function(url) {
+    //     //     // `url` is the download URL for 'images/stars.jpg'
+    //     //     str = url;
+    //     //     // // This can be downloaded directly:
+    //     //     // var xhr = new XMLHttpRequest();
+    //     //     // xhr.responseType = 'blob';
+    //     //     // xhr.onload = function(event) {
+    //     //     //   var blob = xhr.response;
+    //     //     // };
+    //     //     // xhr.open('GET', url);
+    //     //     // xhr.send();
+    //     //     // Or inserted into an <img> element:
+    //     //     // var img = document.getElementById('myimg');
+    //     //     // img.src = url;
+    //     //   })
+    //     //   .catch(function(error) {
+    //     //     // Handle any errors
+    //     //     console.log(error);
+    //     //   });
+    //     // this.setState({ img: str });
+    //     //DOC trial up
+    //     // let url = images
+    //     //   .child('IMG_7772.jpeg')
+    //     //   .then(response => response.ref.getDownloadURL());
+    //     // images
+    //     //   .listAll()
+    //     //   .then(function(result) {
+    //     //     result.items.forEach(function(imageRef) {
+    //     //       // And finally display them
+    //     //       displayImage(imageRef);
+    //     //     });
+    //     //   })
+    //     //   .catch(function(error) {
+    //     //     // Handle any errors
+    //     //     console.log('error');
+    //     //   });
+    //     // function displayImage(imageRef) {
+    //     //   let urlNew = imageRef
+    //     //     .getDownloadURL()
+    //     //     .then(function(url) {
+    //     //       // TODO: Display the image on the UI
+    //     //       console.log('URL', url);
+    //     //       newArr.push(url);
+    //     //     })
+    //     //     .catch(function(error) {
+    //     //       // Handle any errors
+    //     //       console.log('error');
+    //     //     });
+    //     //   console.log('URL NEW', urlNew);
+    //     // }
+    //     //my test working
+    //     // const imageObj = await images.listAll();
+    //     // const imageObjArr = imageObj.items[0];
+    //     // console.log(imageObjArr);
+    //     // console.log('IMAGE OBJ ARRAY', imageObjArr);
+    //     // imageObjArr.forEach(image => {
+    //     //   newArr.push(image.get());
+    //     // });
+    //     // console.log('NEW ARRAY', newArr[0]);
+    //     // this.setState({ urlArray: newArr });
+    //     //test
+    // const images = storage.ref().child('Testing');
     const images = storage.ref().child(this.props.address);
-    const imageObj = await images.listAll();
-    const imageObjArr = imageObj.items;
-    imageObjArr.forEach(image => {
-      newArr.push(image);
-    });
-    this.setState({ urlArray: newArr });
-
-    const image = images.child('IMG_7772.jpeg');
+    const image = images.child(this.props.pictureNames[0]);
     const url = await image.getDownloadURL();
 
-    console.log('HERE IN CDM', imageObj.items[0].name); //, imageObj.items[1].name);
+    console.log('HERE IN CDM', url); //, imageObj.items[1].name);
     this.setState({ img: url });
   };
 
   render() {
-    const { img } = this.state;
-    console.log('STATE', this.state);
+    const { img, urlArray } = this.state;
+    console.log('STATE', this.state, 'PROPS', this.props);
     // console.log('IMG URL', this.state);
     return (
       <div>
+        {/* <img src={urlArray[0]} /> */}
         <img src={img} />
         {/* <img src="https://media.graytvinc.com/images/PERDITA+16+9.jpg" /> */}
       </div>
